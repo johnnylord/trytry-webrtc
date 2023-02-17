1d0a0974f394c3786d568ac7824e103e9dd9a90a
Johnny Liu <johnliu@nvidia.com>
2023-02-17 10:41:08 +0800

    Update installation script for srtp

--- scripts/install_libsrtp.sh
+++ scripts/install_libsrtp.sh
@@ -4,11 +4,14 @@ wget https://github.com/cisco/libsrtp/archive/v2.2.0.tar.gz
 tar xfv v2.2.0.tar.gz
 cd libsrtp-2.2.0
 
+sudo apt-get remove -y 'libsrtp*'
+
 ./configure --prefix=/usr --libdir=/usr/lib64 --enable-openssl
 make shared_library && sudo make install
 
-./configure --prefix=/usr --enable-openssl
-make shared_library && sudo make install
+# If compilation of janus fails, try to put the libsrtp to /usr/lib
+# ./configure --prefix=/usr --enable-openssl
+# make shared_library && sudo make install
 
 cd .. && rm -rf libsrtp-2.2.0 v2.2.0.tar.gz
 
